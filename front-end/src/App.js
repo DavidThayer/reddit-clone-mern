b0e9b80b93111663763666cf445a1f03555d088a
Esther Weon <esther.s.weon@gmail.com>
2018-01-30 13:09:52 -0800

    Create React routes and display all TextPosts on home page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import HomePage from './pages/HomePage';
+import SinglePostPage from './pages/SinglePostPage';
+import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -9,6 +12,11 @@ class App extends Component {
           <h1 className="App-title">reddit</h1>
           <h2 className="App-blurb">The front page of the web</h2>
         </header>
+
+        <Switch>
+          <Route exact path="/" component={ HomePage }/>
+          <Route path="/singlePostPage" component={ SinglePostPage } />
+        </Switch>
       </div>
     );
   }
--- front-end/src/pages/HomePage.js
+++ front-end/src/pages/HomePage.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
 class HomePage extends Component {
+	constructor() {
+		super();
+		this.state = {
+			allMyPosts: []
+		};
+	}
+	componentDidMount() {
+		fetch('http://localhost:8080/api/posts').then((res) => {
+		  return res.json();
+		}).then((json) => {
+		  // Set state to this json response you got back
+		  this.setState({allMyPosts: json})
+		});
+	}
   render() {
     return (
       <p className="HomePage">
 	      Home Page
+		    {/* Render all the posts in your state */}
+		    { this.state.allMyPosts.map(eachPost => {
+		    	return <li key={ eachPost._id }>
+		    		<img height="30" width="30" src={eachPost.thumbnail_image_url}/>
+		    		{ eachPost.title }
+		    	</li>
+		    }) }
       </p>
     );
   }
