5a901de402ca76154264fcaa7c2243fb5ebd8bb7
Esther S. Weon <esther.s.weon@gmail.com>
2018-02-27 09:50:10 -0800

    Update seed.js

--- back-end/db/seed.js
+++ back-end/db/seed.js
@@ -24,42 +24,40 @@ models.Comment.remove({}, function(err, res) {
       return;
     }
     console.log('Created', comments.length, 'comments');
-    return;
+    
+    models.TextPost.remove({}, function(err, res) {
+      if (err) {
+        console.log('Error removing TextPosts', err);
+        return;
+      }
+      console.log('Removed all TextPosts');
+
+      models.TextPost.create([{
+        title: 'Title', 
+        content: 'Content',
+        thumbnail_image_url: 'http://www.readersdigest.ca/wp-content/uploads/2011/01/4-ways-cheer-up-depressed-cat.jpg',
+        votes: 3,
+        comments: [comments[0], comments[1]]
+      }, {
+        title: 'Another Title',
+        content: 'Some more content',
+        thumbnail_image_url: 'http://www.petmd.com/sites/default/files/sleepy-cat-125522297.jpg',
+        votes: 8,
+        comments: [comments[2]]
+      }, {
+        title: 'My Last Title',
+        content: 'Yo some dope content',
+        thumbnail_image_url: 'https://ichef.bbci.co.uk/news/1024/cpsprodpb/693C/production/_95804962_p0517py6.jpg',
+        votes: 12,
+        comments: []
+      }], function(err, posts) {
+        if (err) {
+          console.log('Error creating TextPosts', err);
+          return;
+        }
+        console.log('Created', posts.length, 'posts');
+        return;
+      });
+    });
   })
-});
-
-
-models.TextPost.remove({}, function(err, res) {
-  if (err) {
-    console.log('Error removing TextPosts', err);
-    return;
-  }
-  console.log('Removed all TextPosts');
-
-  models.TextPost.create([{
-    title: 'Title', 
-    content: 'Content',
-    thumbnail_image_url: 'http://www.readersdigest.ca/wp-content/uploads/2011/01/4-ways-cheer-up-depressed-cat.jpg',
-    votes: 3,
-    comments: [commentsData[0], commentsData[1]]
-  }, {
-    title: 'Another Title',
-    content: 'Some more content',
-    thumbnail_image_url: 'http://www.petmd.com/sites/default/files/sleepy-cat-125522297.jpg',
-    votes: 8,
-    comments: [commentsData[2]]
-  }, {
-    title: 'My Last Title',
-    content: 'Yo some dope content',
-    thumbnail_image_url: 'https://ichef.bbci.co.uk/news/1024/cpsprodpb/693C/production/_95804962_p0517py6.jpg',
-    votes: 12,
-    comments: []
-  }], function(err, posts) {
-    if (err) {
-      console.log('Error creating TextPosts', err);
-      return;
-    }
-    console.log('Created', posts.length, 'posts');
-    return;
-  });
 });
